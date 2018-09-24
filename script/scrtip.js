9d4e4be46c8c0285ee6d109319f4b9a3e6f674fa
Konrad <konrad.trade@gmail.com>
2018-09-24 23:39:21 +0200

    Hotfix

--- script/scrtip.js
+++ script/scrtip.js
@@ -11,7 +11,7 @@
 
     const buildElement = (elementType, initialClass, elementText) => {
         
-        const element = createElement(elementType);
+        const element = createElement(elementType)
         addClass(element, initialClass);
         addInput(element, elementText);
         hideElement(element);
@@ -34,11 +34,11 @@
     
 
     const button = buildElement("button", "scroll-button", "Scroll Up" );
-    
+
     // const redButton = buildElement("button", "left-button", "Amigo");
 
 
-    button.addEventListener("click", e => scrollTop(0));
+    button.addEventListener("click", e => scrollToHeight(0));
     window.addEventListener("scroll", e => buttonVisibility(100, button), false);
 
 
