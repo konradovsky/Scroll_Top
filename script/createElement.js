6c5d8fdb2ec58f90e43e73da858b25d7f4fd38fc
Konrad <konrad.trade@gmail.com>
2018-09-30 23:12:30 +0200

    Added script files

---
+++ script/createElement.js
@@ -0,0 +1,40 @@
+// Adding content and styles
+const addClass = (element, cssClass) => element.classList.add(cssClass);
+const addInput = (element, inputText) => element.textContent = inputText;
+
+// Create and render item
+const createElement = element => document.createElement(element);
+const renderElement = element => document.body.appendChild(element);
+
+// Visibility
+const showElement = element => element.classList.remove("hidden");
+const hideElement = element => element.classList.add("hidden");
+    
+// Animation for scroll
+const animateScroll = (height,speed) => {
+    const animation = setInterval(() => {
+        const heightFromTop = document.documentElement.scrollTop;
+        (heightFromTop > height) ? document.documentElement.scrollTop -= speed : clearInterval(animation)
+    }, 1);
+}
+
+const buttonVisibility = (activationHeight, element) => {
+    const heightFromTop = document.documentElement.scrollTop;
+    (heightFromTop >= activationHeight) ? showElement(element) : hideElement(element);
+}
+
+const buildElement = (elementType, initialClass, elementText, activation, speed) => {
+    const element = createElement(elementType);
+
+    addClass(element, initialClass);
+    addInput(element, elementText);
+    hideElement(element);
+    renderElement(element);
+
+    element.addEventListener("click", () => animateScroll(0, speed));
+    window.addEventListener("scroll", () => buttonVisibility(activation, element));
+
+    return element;
+}
+
+export default buildElement;
---
+++ script/script.js
@@ -0,0 +1,15 @@
+import buildElement from './createElement';
+
+(function(){
+
+    // buildElement(
+    //    HTML tag: string,
+    //    added class: string, 
+    //    text: string, 
+    //    height: number
+    //    speed: number
+    // )
+    
+    const button = buildElement("button", "scroll-button", "Scroll Up", 100, 15 );
+
+})();
